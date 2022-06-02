eb875cf24382408cf33a49960f12738554824dd4
Kiran Sahmbi <kiransahmbi@yahoo.ca>
2022-06-02 10:13:23 -0400

    Added Routing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+// Routes
+router.get('/', home);
+router.get('/about-me', aboutme);
+router.get('/projects', projects);
+router.get('/services', services);
+router.get('/contact-me', contactme);
+
+// Controllers
+function home(req, res, next) {
   res.render('index', { title: 'Express' });
-});
+}
+
+function aboutme(req, res, next) {
+  res.render('aboutme', { title: 'Express' });
+}
+
+function projects(req, res, next) {
+  res.render('projects', { title: 'Express' });
+}
+
+function services(req, res, next) {
+  res.render('services', { title: 'Express' });
+}
+
+function contactme(req, res, next) {
+  res.render('contactme', { title: 'Express' });
+}
 
 module.exports = router;
