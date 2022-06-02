7dad453fa0c4170a57a3e0195ed4bdaf93892aa7
Kiran Sahmbi <kiransahmbi@yahoo.ca>
2022-06-02 02:06:06 -0400

    Added Bootstrap

--- config/app.js
+++ config/app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('../routes/index');
-var usersRouter = require('../routes/users');
 
 // Instantiate Express
 var app = express();
@@ -20,9 +19,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
+app.use('/css', express.static(path.join(__dirname, '../node_modules/bootstrap/dist/css')));
+// app.use('/js', express.static(path.join(_dirname, 'node_modules/bootstrap/dist/js')))
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
