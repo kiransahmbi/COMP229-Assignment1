74556271608a8ca28bf4f7817cd8363b0a91a660
Kiran Sahmbi <kiransahmbi@yahoo.ca>
2022-06-19 21:10:05 -0400

    Configure Passport into app.js

--- config/app.js
+++ config/app.js
@@ -4,12 +4,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var flash = require('connect-flash');
+var passport = require('passport');
 
 var indexRouter = require('../routes/index');
+var userRouter = require('../routes/user');
 var contactsRouter = require('../routes/contacts');
 
 // Instantiate Express
 var app = express();
+
+app.use(session({
+  saveUninitialized: true,
+  resave: true,
+  secret: "sessionSecret"
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, '../views'));
@@ -22,9 +32,15 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 app.use('/css', express.static(path.join(__dirname, '../node_modules/bootstrap/dist/css')));
 
+// Sets up passport
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
 // routes
 app.use('/', indexRouter);
 app.use('/contacts', contactsRouter);
+app.use('/user', userRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
