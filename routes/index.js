6a5e437a2d81af722a2316b33386fd10dca0b02b
Kiran Sahmbi <kiransahmbi@yahoo.ca>
2022-06-19 21:15:25 -0400

    Separated Index Routes & Controllers

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,12 @@
 var express = require('express');
 var router = express.Router();
+let controllerIndex = require('../controllers/index');
 
 // Routes
-router.get('/', home);
-router.get('/about-me', aboutme);
-router.get('/projects', projects);
-router.get('/services', services);
-router.get('/contact-me', contactme);
-
-// Controllers
-function home(req, res, next) {
-  res.render('index', {title: "Home"});
-}
-
-function aboutme(req, res, next) {
-  res.render('aboutme', {title: "About Me"});
-}
-
-function projects(req, res, next) {
-  res.render('projects', {title: "Projects"});
-}
-
-function services(req, res, next) {
-  res.render('services', {title: "Services"});
-}
-
-function contactme(req, res, next) {
-  res.render('contactme', {title: "Contact Me"});
-}
+router.get('/', controllerIndex.home);
+router.get('/about-me', controllerIndex.aboutme);
+router.get('/projects', controllerIndex.projects);
+router.get('/services', controllerIndex.services);
+router.get('/contact-me', controllerIndex.contactme);
 
 module.exports = router;
